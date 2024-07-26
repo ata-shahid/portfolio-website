1510d07a440709699e6770604a0bd266d8fa0d0a
ashah001 <ataul.shahid@student.hs-rm.de>
2024-07-26 19:11:54 +0200

    update skills section

--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -4,32 +4,33 @@ import Language from './Language'
 
 const Skills = () => {
   return (
-    <div className='pt-[4rem] md:pt-[8rem] pb-[5rem] bg-[#09101a]'>
+    <div className='pt-[2rem] md:pt-[8rem] pb-[5rem] bg-[#09101a]'>
       <h1 className='heading'>Education &amp; <span className='text-yellow-400'>Skills</span></h1>
       
       {/* Text Containers Row */}
-      <div className='flex flex-wrap w-[80%] mx-auto pt-[1rem] md:pt-[2rem] gap-[2rem] items-start mt-8'>
-        <div className='flex-1'>
-          <Skill 
-            title="Front-End Development" 
-            description="Experience with HTML, CSS, and JavaScript. Proficient in creating responsive and interactive web applications."
-          />
-          <Skill 
-            title="UI/UX Design" 
-            description="Skilled in designing user-friendly interfaces with tools like Sketch, Figma, and Adobe XD. Focus on user experience and accessibility."
-          />
-        </div>
-        <div className='flex-1'>
-          <Skill 
-            title="React & Next.js" 
-            description="Developed multiple projects using React and Next.js. Expertise in building dynamic, high-performance web applications."
-          />
-          <Skill 
-            title="Back-End Development" 
-            description="Proficient in server-side technologies such as Node.js, Express, and databases like MongoDB and MySQL."
-          />
-        </div>
-      </div>
+<div className='flex flex-wrap w-[80%] mx-auto pt-[1rem] md:pt-[2rem] gap-[2rem] items-start mt-8'>
+  <div className='flex-1'>
+    <Skill 
+      title="Frontend-Entwicklung" 
+      description="Entwicklung responsiver Webanwendungen und intuitiver Benutzeroberflächen zur Verbesserung der Nutzererfahrung."
+    />
+    <Skill 
+      title="UI/UX Design" 
+      description="Erstellung ästhetischer und funktionaler Designs durch Nutzerforschung und iteratives Prototyping."
+    />
+  </div>
+  <div className='flex-1'>
+    <Skill 
+      title="React & Next.js" 
+      description="Entwicklung von Webanwendungen mit modernen Frameworks. Erstellung flexibler und leistungsstarker Frontend-Lösungen."
+    />
+    <Skill 
+      title="Back-End Entwicklung" 
+      description="Integration von Datenbanken und APIs zur Unterstützung von Webanwendungen."
+    />
+  </div>
+</div>
+
       
       {/* Bars Containers Row */}
       <div className='flex flex-wrap w-[80%] mx-auto  gap-[2rem] items-start mt-8'>
