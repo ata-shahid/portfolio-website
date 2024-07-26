5d23f3f6a47e334791ab074f33c3cb05c8f6d6c1
ashah001 <ataul.shahid@student.hs-rm.de>
2024-07-26 21:23:39 +0200

    add offset value

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -7,17 +7,18 @@ interface NavbarProps {
 }
 
 const Navbar = ({ openNav }: NavbarProps) => {
+  const offsetValue = -70; // Adjust this value based on your header height
+
   return (
     <div className='w-[100%] fixed z-[10000] top-0 h-[12vh] bg-[#141c27] shadow-md'>
       <div className='flex items-center justify-between w-[80%] mx-auto h-[100%]'>
         <h1 className=' flex-[0.6] text-white font-bold text-2xl'>Portfolio</h1>
 
-        <Link to="hero" smooth={true} duration={900} className='nav-link text-white'>Home</Link>
-        <Link to="about" smooth={true} duration={900} className='nav-link text-white'>About</Link>
-        <Link to="skills" smooth={true} duration={900} className='nav-link text-white'>Skills</Link>
-        <Link to="projects" smooth={true} duration={900} className='nav-link text-white'>Projects</Link>
-       
-        <Link to="contact" smooth={true} duration={900} className='nav-link text-white'>Contact</Link>
+        <Link to="hero" smooth={true} duration={900} offset={offsetValue} className='nav-link text-white'>Home</Link>
+        <Link to="about" smooth={true} duration={900} offset={offsetValue} className='nav-link text-white'>About</Link>
+        <Link to="skills" smooth={true} duration={900} offset={offsetValue} className='nav-link text-white'>Skills</Link>
+        <Link to="projects" smooth={true} duration={900} offset={offsetValue} className='nav-link text-white'>Projects</Link>
+        <Link to="contact" smooth={true} duration={900} offset={offsetValue} className='nav-link text-white'>Contact</Link>
         <div onClick={openNav}>
           <Bars3Icon className='w-8 md:hidden h-8 cursor-pointer text-yellow-300' />
         </div>
